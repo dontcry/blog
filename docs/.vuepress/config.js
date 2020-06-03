ab19066759870929446a78f14adf3e16a5f8acc7
李浩然 <li_haoran@hotmail.com>
2020-06-03 20:10:37 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,8 @@
 module.exports = {
     base: '/blog/',
-    title: 'lihr\'s blog',
-    description: '李浩然的博客',
-    head: [
-        ['link', { rel: 'icon', href: '/img/logo.ico' }],
-        ['link', { rel: 'manifest', href: '/manifest.json' }],
-    ]
+    title: 'lihr\'s blog ',
+    description: 'lihr的博客',
+    dest: './docs/.vuepress/dist',
+    ga: '',
+    evergreen: true,
 }
