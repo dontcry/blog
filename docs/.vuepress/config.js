a24551d301a555393b7e31de17d704b6f06eebf3
李浩然 <li_haoran@hotmail.com>
2020-06-04 23:59:42 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,6 +13,10 @@ module.exports = {
         lineNumbers: true // 代码块显示行号
     },
     themeConfig: {
+        logo: './img/i5.png',
+        authorAvatar: './img/artist.png',
+        author: 'LiHR',
+        huawei: true,
         nav: [
             { text: 'Home', link: '/', icon: 'reco-home' },
             { text: '时间轴', link: '/timeline/', icon: 'reco-date' },
@@ -24,6 +28,7 @@ module.exports = {
                     { text: 'English', link: '/languages/English' }
                 ], icon: 'reco-date'
             },
+            { text: '文章', link: '/NewBeginning', icon: 'reco-date' },
             { text: '百度', link: 'https://www.baidu.com', icon: 'reco-date' },
         ],
         sidebarDepth: 2,
---
+++ docs/NewBeginning.md
@@ -0,0 +1,5 @@
+---
+title: New Beginning
+date: 2020-06-04
+categories: article 
+---
